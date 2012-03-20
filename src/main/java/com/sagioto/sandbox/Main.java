b98b8fa5795db8e787bfbd302d8a968df00aa938
Sagi Bernstein <sagioto@gmail.com>
2012-03-20 14:48:38 +0200

    try again commit from eclipse
    Signed-off-by: Sagi Bernstein <sagioto@gmail.com>

--- src/main/java/com/sagioto/sandbox/Main.java
+++ src/main/java/com/sagioto/sandbox/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
     	System.out.println("Hello, git!" );
     	System.out.println("commit from eclipse!" );
+    	System.out.println("okay, now try this" );
     }
 
 
